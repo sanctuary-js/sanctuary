1ba88a47690f4b5d78e63e8c86ac0c712d7a53ab
stefano <svozza@gmail.com>
2015-11-07 10:56:33 +0000

    add allPass and anyPass

--- index.js
+++ index.js
@@ -1624,6 +1624,48 @@
     return pred(x) ? f(x) : g(x);
   });
 
+  //# allPass :: [a -> Boolean] -> a -> Boolean
+  //.
+  //. Takes an array of unary predicates and a value of any type
+  //. and returns `true` if all the predicates pass; `false` otherwise.
+  //. None of the subsequent predicates will be evaluated after the
+  //. first failed predicate.
+  //.
+  //. ```javascript
+  //. > S.allPass([S.test(/q/), S.test(/u/), S.test(/i/)], 'quiessence')
+  //. true
+  //.
+  //. > S.allPass([S.test(/q/), S.test(/u/), S.test(/i/)], 'fissiparous')
+  //. false
+  //. ```
+  S.allPass = def('allPass', [Array, a], function(preds, x) {
+    for (var idx = 0; idx < preds.length; idx += 1) {
+      if (!preds[idx](x)) return false;
+    }
+    return true;
+  });
+
+  //# anyPass :: [a -> Boolean] -> a -> Boolean
+  //.
+  //. Takes an array of unary predicates and a value of any type
+  //. and returns `true` if any of the predicates pass; `false` otherwise.
+  //. None of the subsequent predicates will be evaluated after the
+  //. first passed predicate.
+  //.
+  //. ```javascript
+  //. > S.anyPass([S.test(/q/), S.test(/u/), S.test(/i/)], 'incandescent')
+  //. true
+  //.
+  //. > S.anyPass([S.test(/q/), S.test(/u/), S.test(/i/)], 'empathy')
+  //. false
+  //. ```
+  S.anyPass = def('anyPass', [Array, a], function(preds, x) {
+    for (var idx = 0; idx < preds.length; idx += 1) {
+      if (preds[idx](x)) return true;
+    }
+    return false;
+  });
+
   //. ### List
 
   //# slice :: Integer -> Integer -> [a] -> Maybe [a]
