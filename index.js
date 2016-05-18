e76b1c327b854a5ec24f176f6102868d5cf84086
David Chambers <dc@davidchambers.me>
2016-05-18 10:57:31 -0700

    array: add S.range

--- index.js
+++ index.js
@@ -2670,6 +2670,32 @@
         return result;
       });
 
+  //# range :: Integer -> Integer -> Array Integer
+  //.
+  //. Returns an array of consecutive integers starting with the first argument
+  //. and ending with the second argument minus one. Returns `[]` if the second
+  //. argument is less than or equal to the first argument.
+  //.
+  //. ```javascript
+  //. > S.range(0, 10)
+  //. [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  //.
+  //. > S.range(-5, 0)
+  //. [-5, -4, -3, -2, -1]
+  //.
+  //. > S.range(0, -5)
+  //. []
+  //. ```
+  S.range =
+  def('range',
+      {},
+      [$.Integer, $.Integer, $.Array($.Integer)],
+      function(from, to) {
+        var result = [];
+        for (var n = from; n < to; n += 1) result.push(n);
+        return result;
+      });
+
   //. ### Object
 
   //# prop :: Accessible a => String -> a -> b
---
+++ test/range.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var throws = require('assert').throws;
+
+var eq = require('./utils').eq;
+var errorEq = require('./utils').errorEq;
+var S = require('..');
+
+
+describe('range', function() {
+
+  it('is a binary function', function() {
+    eq(typeof S.range, 'function');
+    eq(S.range.length, 2);
+  });
+
+  it('type checks its arguments', function() {
+    throws(function() { S.range(0.5); },
+           errorEq(TypeError,
+                   'Invalid value\n' +
+                   '\n' +
+                   'range :: Integer -> Integer -> Array Integer\n' +
+                   '         ^^^^^^^\n' +
+                   '            1\n' +
+                   '\n' +
+                   '1)  0.5 :: Number, FiniteNumber, NonZeroFiniteNumber, ValidNumber\n' +
+                   '\n' +
+                   'The value at position 1 is not a member of ‘Integer’.\n'));
+
+    throws(function() { S.range(0, 0.5); },
+           errorEq(TypeError,
+                   'Invalid value\n' +
+                   '\n' +
+                   'range :: Integer -> Integer -> Array Integer\n' +
+                   '                    ^^^^^^^\n' +
+                   '                       1\n' +
+                   '\n' +
+                   '1)  0.5 :: Number, FiniteNumber, NonZeroFiniteNumber, ValidNumber\n' +
+                   '\n' +
+                   'The value at position 1 is not a member of ‘Integer’.\n'));
+  });
+
+  it('returns an array of consecutive integers', function() {
+    eq(S.range(0, 0), []);
+    eq(S.range(0, 10), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    eq(S.range(0, -10), []);
+    eq(S.range(-2, -1), [-2]);
+    eq(S.range(-2, 3), [-2, -1, 0, 1, 2]);
+  });
+
+  it('is curried', function() {
+    eq(S.range(0).length, 1);
+    eq(S.range(0)(10), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+
+});
