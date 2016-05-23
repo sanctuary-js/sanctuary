41562c9139b36536efd267c4220b0288ce4be26a
Tobias Pflug <tobias.pflug@gmail.com>
2016-05-23 21:14:01 +0200

    Add S.reverse

---
+++ test/reverse.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var throws = require('assert').throws;
+
+var eq = require('./utils').eq;
+var errorEq = require('./utils').errorEq;
+var S = require('..');
+
+
+describe('reverse', function() {
+
+  it('is a unary function', function() {
+    eq(typeof S.reverse, 'function');
+    eq(S.reverse.length, 1);
+  });
+
+  it('type checks its arguments', function() {
+    throws(function() { S.reverse({answer: 42}); },
+           errorEq(TypeError,
+                   'Invalid value\n' +
+                   '\n' +
+                   'reverse :: List a -> List a\n' +
+                   '           ^^^^^^\n' +
+                   '             1\n' +
+                   '\n' +
+                   '1)  {"answer": 42} :: Object, StrMap Number, StrMap FiniteNumber, StrMap NonZeroFiniteNumber, StrMap Integer, StrMap ValidNumber\n' +
+                   '\n' +
+                   'The value at position 1 is not a member of ‘List a’.\n'));
+
+  });
+
+  it('reverses arrays', function() {
+    eq(S.reverse([]), []);
+    eq(S.reverse([1, 2, 3]), [3, 2, 1]);
+    eq(S.reverse(['1', '2', '3']), ['3', '2', '1']);
+  });
+
+  it('reverses strings', function() {
+    eq(S.reverse(''), '');
+    eq(S.reverse('123'), '321');
+  });
+
+});
