be1f26eac956f77cd19bc75c9e5ee5946164ae16
wennergr <wennergr@users.noreply.github.com>
2016-06-06 19:49:40 -0700

    Add S.fromEither to pull out values from either
    S.fromEither :: b -> Either a b -> b
    
    Closes: #130

--- index.js
+++ index.js
@@ -1876,6 +1876,24 @@
       [$Either(a, b), $.Boolean],
       prop('isRight'));
 
+  //# fromEither :: b -> Either a b -> b
+  //.
+  //. Takes a default value and an Either, and returns the Right value
+  //. if the Either is a Right; the default value otherwise.
+  //.
+  //. ```javascript
+  //. > S.fromEither(0, S.Right(42))
+  //. 42
+  //.
+  //. > S.fromEither(0, S.Left(42))
+  //. 0
+  //. ```
+  S.fromEither =
+  def('fromEither',
+      {},
+      [b, $Either(a, b), b],
+      function(x, either) { return either.isRight ? either.value : x; });
+
   //# either :: (a -> c) -> (b -> c) -> Either a b -> c
   //.
   //. Takes two functions and an Either, and returns the result of
