951a7e7a12c9ed87e0f9689d09a14cc6e401c134
Gabe Johnson <gijohnson105@gmail.com>
2017-02-26 20:48:44 -0600

    foldable: add S.takeWhile and S.dropWhile

---
+++ test/dropWhile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var S = require('..');
+
+var eq = require('./internal/eq');
+
+
+test('dropWhile', function() {
+
+  eq(typeof S.dropWhile, 'function');
+  eq(S.dropWhile.length, 2);
+  eq(S.dropWhile.toString(), 'dropWhile :: (Foldable f, Alternative f) => (a -> Boolean) -> f a -> f a');
+
+  eq(S.dropWhile(S.odd, [3, 3, 3, 7, 6, 3, 5, 4]), [6, 3, 5, 4]);
+  eq(S.dropWhile(S.even, [3, 3, 3, 7, 6, 3, 5, 4]), [3, 3, 3, 7, 6, 3, 5, 4]);
+  eq(S.dropWhile(S.odd, []), []);
+
+  eq(S.dropWhile(S.odd, S.Just(1)), S.Nothing);
+  eq(S.dropWhile(S.even, S.Just(1)), S.Just(1));
+  eq(S.dropWhile(S.odd, S.Nothing), S.Nothing);
+
+});
