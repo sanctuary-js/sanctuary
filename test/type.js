7633b1183289b887a2b10aa92df80dcddc305c42
David Chambers <dc@davidchambers.me>
2017-05-22 08:36:38 +0200

    sanctuary-type-identifiers@2.0.1
    This commit updates the return type of S.type. Those wishing to access
    unparsed type identifiers can use sanctuary-type-identifiers directly.

--- test/type.js
+++ test/type.js
@@ -11,36 +11,36 @@ test('type', function() {
 
   eq(typeof S.type, 'function');
   eq(S.type.length, 1);
-  eq(S.type.toString(), 'type :: Any -> String');
+  eq(S.type.toString(), 'type :: Any -> { name :: String, namespace :: Maybe String, version :: Integer }');
 
   var args = (function() { return arguments; }());
-  eq(S.type(args),                'Arguments');
-  eq(S.type([]),                  'Array');
-  eq(S.type(false),               'Boolean');
-  eq(S.type(new Date(0)),         'Date');
-  eq(S.type(new TypeError()),     'Error');
-  eq(S.type(function() {}),       'Function');
-  eq(S.type(null),                'Null');
-  eq(S.type(0),                   'Number');
-  eq(S.type(/(?:)/),              'RegExp');
-  eq(S.type(''),                  'String');
-  eq(S.type(undefined),           'Undefined');
-  eq(S.type(new Boolean(false)),  'Boolean');
-  eq(S.type(new Number(0)),       'Number');
-  eq(S.type(new String('')),      'String');
+  eq(S.type(args),                {namespace: S.Nothing, name: 'Arguments', version: 0});
+  eq(S.type([]),                  {namespace: S.Nothing, name: 'Array', version: 0});
+  eq(S.type(false),               {namespace: S.Nothing, name: 'Boolean', version: 0});
+  eq(S.type(new Date(0)),         {namespace: S.Nothing, name: 'Date', version: 0});
+  eq(S.type(new TypeError()),     {namespace: S.Nothing, name: 'Error', version: 0});
+  eq(S.type(function() {}),       {namespace: S.Nothing, name: 'Function', version: 0});
+  eq(S.type(null),                {namespace: S.Nothing, name: 'Null', version: 0});
+  eq(S.type(0),                   {namespace: S.Nothing, name: 'Number', version: 0});
+  eq(S.type(/(?:)/),              {namespace: S.Nothing, name: 'RegExp', version: 0});
+  eq(S.type(''),                  {namespace: S.Nothing, name: 'String', version: 0});
+  eq(S.type(undefined),           {namespace: S.Nothing, name: 'Undefined', version: 0});
+  eq(S.type(new Boolean(false)),  {namespace: S.Nothing, name: 'Boolean', version: 0});
+  eq(S.type(new Number(0)),       {namespace: S.Nothing, name: 'Number', version: 0});
+  eq(S.type(new String('')),      {namespace: S.Nothing, name: 'String', version: 0});
 
-  eq(S.type(S.Left(42)),  'sanctuary/Either');
-  eq(S.type(S.Right(42)), 'sanctuary/Either');
-  eq(S.type(S.Nothing),   'sanctuary/Maybe');
-  eq(S.type(S.Just(42)),  'sanctuary/Maybe');
+  eq(S.type(S.Left(42)),          {namespace: S.Just('sanctuary'), name: 'Either', version: 0});
+  eq(S.type(S.Right(42)),         {namespace: S.Just('sanctuary'), name: 'Either', version: 0});
+  eq(S.type(S.Nothing),           {namespace: S.Just('sanctuary'), name: 'Maybe', version: 0});
+  eq(S.type(S.Just(42)),          {namespace: S.Just('sanctuary'), name: 'Maybe', version: 0});
 
   function Gizmo() {}
-  Gizmo['@@type'] = 'gadgets/Gizmo';
+  Gizmo['@@type'] = 'gadgets/Gizmo@42';
 
-  eq(S.type(new Gizmo()), 'gadgets/Gizmo');
-  eq(S.type(Gizmo), 'Function');
-  eq(S.type(Gizmo.prototype), 'Object');
+  eq(S.type(new Gizmo()),         {namespace: S.Just('gadgets'), name: 'Gizmo', version: 42});
+  eq(S.type(Gizmo),               {namespace: S.Nothing, name: 'Function', version: 0});
+  eq(S.type(Gizmo.prototype),     {namespace: S.Nothing, name: 'Object', version: 0});
 
-  eq(S.type(vm.runInNewContext('[1, 2, 3]')), 'Array');
+  eq(S.type(vm.runInNewContext('[1, 2, 3]')), {namespace: S.Nothing, name: 'Array', version: 0});
 
 });
