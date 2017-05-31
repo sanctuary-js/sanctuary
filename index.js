1d29853baf460bd512a6fdc53b2d215959e6c943
David Chambers <dc@davidchambers.me>
2017-05-31 20:46:49 +0200

    Version 0.13.1

--- index.js
+++ index.js
@@ -286,7 +286,7 @@
 
   //  readmeUrl :: String -> String
   function readmeUrl(id) {
-    var version = '0.13.0';  // updated programmatically
+    var version = '0.13.1';  // updated programmatically
     return 'https://github.com/sanctuary-js/sanctuary/tree/v' + version +
            '#' + id;
   }
