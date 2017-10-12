38c850a1178b29bd2f95decaeae1651f7b3aae5b
David Chambers <dc@davidchambers.me>
2017-10-12 19:15:01 +0200

    logic: generalize S.allPass and S.anyPass

--- test/allPass.js
+++ test/allPass.js
@@ -1,19 +1,28 @@
 'use strict';
 
-var S = require('..');
+var S = require('./internal/sanctuary');
 
+var List = require('./internal/List');
 var eq = require('./internal/eq');
+
+
+var Cons = List.Cons;
+var Nil = List.Nil;
 
 
 test('allPass', function() {
 
   eq(typeof S.allPass, 'function');
   eq(S.allPass.length, 2);
-  eq(S.allPass.toString(), 'allPass :: Array (a -> Boolean) -> a -> Boolean');
+  eq(S.allPass.toString(), 'allPass :: Foldable f => f (a -> Boolean) -> a -> Boolean');
 
   eq(S.allPass([], 'abacus'), true);
   eq(S.allPass([S.test(/a/), S.test(/b/), S.test(/c/)], 'abacus'), true);
   eq(S.allPass([S.test(/a/), S.test(/b/), S.test(/c/)], 'banana'), false);
+
+  eq(S.allPass(Nil, 'abacus'), true);
+  eq(S.allPass(Cons(S.test(/a/), Cons(S.test(/b/), Cons(S.test(/c/), Nil))), 'abacus'), true);
+  eq(S.allPass(Cons(S.test(/a/), Cons(S.test(/b/), Cons(S.test(/c/), Nil))), 'banana'), false);
 
   var e = false;
   eq(S.allPass([S.test(/a/), function() { e = true; }], 'monkey'), false);
--- test/anyPass.js
+++ test/anyPass.js
@@ -1,19 +1,28 @@
 'use strict';
 
-var S = require('..');
+var S = require('./internal/sanctuary');
 
+var List = require('./internal/List');
 var eq = require('./internal/eq');
+
+
+var Cons = List.Cons;
+var Nil = List.Nil;
 
 
 test('anyPass', function() {
 
   eq(typeof S.anyPass, 'function');
   eq(S.anyPass.length, 2);
-  eq(S.anyPass.toString(), 'anyPass :: Array (a -> Boolean) -> a -> Boolean');
+  eq(S.anyPass.toString(), 'anyPass :: Foldable f => f (a -> Boolean) -> a -> Boolean');
 
-  eq(S.anyPass([], 'narwhal'), false);
+  eq(S.anyPass([], 'dolphin'), false);
+  eq(S.anyPass([S.test(/a/), S.test(/b/), S.test(/c/)], 'dolphin'), false);
   eq(S.anyPass([S.test(/a/), S.test(/b/), S.test(/c/)], 'narwhal'), true);
-  eq(S.anyPass([S.test(/a/), S.test(/b/), S.test(/c/)], 'gorilla'), true);
+
+  eq(S.anyPass(Nil, 'dolphin'), false);
+  eq(S.anyPass(Cons(S.test(/a/), Cons(S.test(/b/), Cons(S.test(/c/), Nil))), 'dolphin'), false);
+  eq(S.anyPass(Cons(S.test(/a/), Cons(S.test(/b/), Cons(S.test(/c/), Nil))), 'narwhal'), true);
 
   var e = false;
   eq(S.anyPass([S.test(/a/), function() { e = true; }], 'narwhal'), true);
