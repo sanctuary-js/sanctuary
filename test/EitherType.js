465e4c5b075e9050e6f4e217d66f974aa54e7193
David Chambers <dc@davidchambers.me>
2018-05-28 19:40:42 +0200

    test: add missing metadata assertions

--- test/EitherType.js
+++ test/EitherType.js
@@ -9,6 +9,10 @@ var eq = require ('./internal/eq');
 
 test ('EitherType', function() {
 
+  eq (typeof S.EitherType) ('function');
+  eq (S.EitherType.length) (1);
+  eq (S.show (S.EitherType)) ('Either :: Type -> Type -> Type');
+
   eq (S.is (S.EitherType ($.String) ($.Number)) (S.Left ('Error'))) (true);
   eq (S.is (S.EitherType ($.String) ($.Number)) (S.Right (42))) (true);
   eq (S.is (S.EitherType ($.String) ($.Number)) (S.Right ('42'))) (false);
--- test/MaybeType.js
+++ test/MaybeType.js
@@ -9,6 +9,10 @@ var eq = require ('./internal/eq');
 
 test ('MaybeType', function() {
 
+  eq (typeof S.MaybeType) ('function');
+  eq (S.MaybeType.length) (1);
+  eq (S.show (S.MaybeType)) ('Maybe :: Type -> Type');
+
   eq (S.is (S.MaybeType ($.Number)) (S.Nothing)) (true);
   eq (S.is (S.MaybeType ($.Number)) (S.Just (42))) (true);
   eq (S.is (S.MaybeType ($.Number)) (S.Just ('42'))) (false);
