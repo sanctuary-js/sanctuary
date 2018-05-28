3e01e277b2c725a31a1106ad7d16ae846b78e339
David Chambers <dc@davidchambers.me>
2018-05-28 19:13:17 +0200

    test: use S.is

--- test/MaybeType.js
+++ test/MaybeType.js
@@ -9,10 +9,10 @@ var eq = require ('./internal/eq');
 
 test ('MaybeType', function() {
 
-  eq ($.test ($.env) (S.MaybeType ($.Number)) (S.Nothing)) (true);
-  eq ($.test ($.env) (S.MaybeType ($.Number)) (S.Just (42))) (true);
-  eq ($.test ($.env) (S.MaybeType ($.Number)) (S.Just ('42'))) (false);
-  eq ($.test ($.env) (S.MaybeType ($.Number)) (S.Right (42))) (false);
-  eq ($.test ($.env) (S.MaybeType ($.Number)) (null)) (false);
+  eq (S.is (S.MaybeType ($.Number)) (S.Nothing)) (true);
+  eq (S.is (S.MaybeType ($.Number)) (S.Just (42))) (true);
+  eq (S.is (S.MaybeType ($.Number)) (S.Just ('42'))) (false);
+  eq (S.is (S.MaybeType ($.Number)) (S.Right (42))) (false);
+  eq (S.is (S.MaybeType ($.Number)) (null)) (false);
 
 });
