f9c1e87ff8f2f0c201848146de2809202155af07
Asad Saeeduddin <masaeedu@gmail.com>
2018-06-13 01:17:38 -0400

    Add test

---
+++ test/intercalate.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var S = require ('..');
+
+var eq = require ('./internal/eq');
+
+test ('intercalate', function() {
+  eq (typeof S.intercalate) ('function');
+  eq (S.intercalate.length) (1);
+  eq (S.show (S.intercalate)) (
+    'intercalate :: Monoid b => TypeRep b -> b -> Array b -> b'
+  );
+
+  var sep = ' mississippi, ';
+  var expects = [
+    [
+      ['one', 'two', 'three', 'four'],
+      'one mississippi, two mississippi, three mississippi, four'
+    ],
+    [['one'], 'one'],
+    [[], '']
+  ];
+
+  expects.map (([l, r]) => eq (S.intercalate (String) (sep) (l)) (r));
+});
