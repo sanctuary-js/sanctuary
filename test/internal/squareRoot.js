1bd5b49c045a9ff3f9229845d04707db2f5b9b6a
David Chambers <dc@davidchambers.me>
2019-03-07 13:06:57 +0100

    test: remove redundant ADT tests

--- test/internal/squareRoot.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var S = require ('../..');
-
-//  squareRoot :: Number -> Either String Number
-module.exports = function squareRoot(n) {
-  return n < 0 ? S.Left ('Cannot represent square root of negative number')
-               : S.Right (Math.sqrt (n));
-};
